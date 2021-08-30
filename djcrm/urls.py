6696711dc993c9b3857fd193e7a417a987a88766
Alexander Skornyakov <a1212sk@gmail.com>
2021-08-30 10:23:56 +0300

    add details view

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from leads.views import home_page
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page)
+    path('leads/', include('leads.urls', namespace="leads"))
 ]
--- leads/views.py
+++ leads/views.py
@@ -4,10 +4,15 @@ from django.http import HttpResponse
 from .models import Lead
 
 
-def home_page(request):
+def lead_list(request):
     # return HttpResponse("Home page")
     leads = Lead.objects.all()
     context = {
         "leads":leads
     }
-    return render(request, "second_page.html", context)
+    return render(request, "leads/lead_list.html", context)
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = { "lead":lead }
+    return render(request, "leads/lead_detail.html", context)
