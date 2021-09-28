63bdd42f8f3352cdda6dd0769795aba7e2e9b81b
Alexander Skornyakov <a1212sk@gmail.com>
2021-09-28 18:36:05 +0300

    make leads accessible to only logged in users

--- leads/views.py
+++ leads/views.py
@@ -9,23 +9,24 @@ from .forms import CustomUserCreationForm, LeadForm
 from .forms import LeadModelForm
 from django.core.mail import send_mail
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class LandingPageView(TemplateView):
     template_name = "landing.html"
 
-class LeadListView(ListView):
+class LeadListView(LoginRequiredMixin, ListView):
     template_name = "leads/lead_list.html"
     queryset = Lead.objects.all()
     context_object_name = "leads"
 
-class LeadDetailView(DetailView):
+class LeadDetailView(LoginRequiredMixin, DetailView):
     template_name = "leads/lead_detail.html"
     queryset = Lead.objects.all()
     context_object_name = "lead"
 
 
-class LeadCreateView(CreateView):
+class LeadCreateView(LoginRequiredMixin, CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadModelForm
 
@@ -41,7 +42,7 @@ class LeadCreateView(CreateView):
         )
         return super(LeadCreateView, self).form_valid(form)
 
-class LeadUpdateView(UpdateView):
+class LeadUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "leads/lead_update.html"
     form_class = LeadModelForm
     queryset = Lead.objects.all()
@@ -49,7 +50,7 @@ class LeadUpdateView(UpdateView):
     def get_success_url(self):
         return reverse("leads:lead-list")
 
-class LeadDeleteView(DeleteView):
+class LeadDeleteView(LoginRequiredMixin, DeleteView):
     template_name = "leads/lead_delete.html"
     queryset = Lead.objects.all()
     
